1765ff06c4ffd5bd6d57b9852f8efaebc4697714
Alex Somera <alexsomera@gmail.com>
2025-08-17 12:34:12 -0300

    Finalização do projeto

---
+++ assets/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export const commonStyles = StyleSheet.create({
+    container: {
+        padding: 10,
+        paddingBlock: 30,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 22,
+        marginBottom: 20,
+    },
+    itemContainer: {
+        flex: 1,
+        margin: 5,
+    },
+    item: {
+        flex: 1,
+        margin: 5,
+        marginBottom: 10,
+    },
+    image: {
+        width: '100%',
+        height: 100,
+    },
+    thumbTitle: {
+        marginTop: 5,
+    },
+});
